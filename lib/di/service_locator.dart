e96283357a134104653e76f54264cfd4057f299f
fabriciocoelho <fabriciocoelho03082000@hotmail.com>
2025-08-15 18:40:57 -0400

    RecipeDetail, tabelas e favorites

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:app4_receitas/data/repositories/recipe_repository.dart';
 import 'package:app4_receitas/data/services/recipe_service.dart';
+import 'package:app4_receitas/ui/recpedetail/recipe_detail_viewmodel.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:app4_receitas/ui/recipes/recipes_viewmodel.dart';
@@ -15,8 +16,11 @@ Future<void> setupDependencies() async {
 
   // Recipe Repository
   getIt.registerLazySingleton<RecipeRepository>(() => RecipeRepository());
+  getIt.registerLazySingleton<RecipeRepository>(() => RecipeRepository());
 
   // Recipe ViewModel
   getIt.registerLazySingleton<RecipesViewModel>(() => RecipesViewModel());
-
+  getIt.registerLazySingleton<RecipeDetailViewModel>(
+    () => RecipeDetailViewModel(),
+  );
 }
